768d92ce8ce2ab02e06fea4bb22198cf7e4124cf
Arjit Gautam <arjitgautamw2015@gmail.com>
2025-04-12 03:57:35 +0530

    Fixed the app flow

--- src/BookMyMovieApp.java
+++ src/BookMyMovieApp.java
@@ -1,6 +1,22 @@
+import service.BookMyMoviesSystem;
+
+import java.util.Scanner;
+
 public class BookMyMovieApp {
     public static void main(String[] args) {
-        String a = System.getenv("DB_URL");
-        System.out.println(a);
+        BookMyMoviesSystem sys = new BookMyMoviesSystem();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your city: ");
+        String city = sc.nextLine();
+        int movieId = sys.displayMovies(city);
+
+        if(movieId == -1 || !sys.displayTheaters(city, movieId)){ //no theater found. Exit
+            return;
+        }
+
+        System.out.println("Select Theater No.: ");
+        int theaterId = sc.nextInt();
+        sys.displayShows(movieId, theaterId);
     }
 }
