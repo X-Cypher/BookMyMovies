f752977c135031ffa01347a0d30d31dfa8909223
Arjit Gautam <arjitgautamw2015@gmail.com>
2025-04-13 03:44:22 +0530

    Changes

--- src/BookMyMovieApp.java
+++ src/BookMyMovieApp.java
@@ -1,5 +1,8 @@
 import service.BookMyMoviesSystem;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class BookMyMovieApp {
@@ -11,16 +14,41 @@ public class BookMyMovieApp {
         sys.displayUsers();
         System.out.println("Select your User Id");
         int userId = sc.nextInt();
-        System.out.println("Enter your city: ");
-        String city = sc.next();
-        int movieId = sys.displayMovies(city);
+        sys.authenticateUser(userId);
 
-        if(movieId == -1 || !sys.displayTheaters(city, movieId)){ //no theater found. Exit
-            return;
+        System.out.println("Enter 1 to Book. 0 to Cancel Booking");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        if(choice == 1) {
+            System.out.println("Enter your city: ");
+            String city = sc.nextLine();
+            int movieId = sys.displayMovies(city);
+
+            sys.displayTheaters(city, movieId);
+            System.out.println("Select Theater No.: ");
+            int theaterId = sc.nextInt();
+            sys.displayShows(movieId, theaterId);
+
+            System.out.println("Select the Show Id with which you want to proceed: ");
+            int showId = sc.nextInt();
+            HashSet<Integer> validSeats = sys.displayAvailableSeats(showId, theaterId);
+
+            List<Integer> selectedSeatsId = sys.selectSeats(validSeats);
+            List<String> selectedSeats = sys.findSeatsById(selectedSeatsId);
+
+            if (selectedSeats == null || selectedSeats.isEmpty()) {
+                System.out.println("No seats selected. Kindly start a new booking....");
+                return;
+            }
+
+            System.out.println("Your Selected Seats: " + selectedSeats);
+            sys.bookTicket(userId, showId, selectedSeats);
+
+        } else if(choice == 0){
+            sys.cancelBooking();
+        } else{
+            System.out.println("Invalid Selection");
         }
-
-        System.out.println("Select Theater No.: ");
-        int theaterId = sc.nextInt();
-        sys.displayShows(movieId, theaterId);
     }
 }
--- src/service/BookMyMoviesSystem.java
+++ src/service/BookMyMoviesSystem.java
@@ -3,6 +3,8 @@ package service;
 import config.DatabaseConfig;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
@@ -37,6 +39,22 @@ public class BookMyMoviesSystem {
         }
     }
 
+    public void authenticateUser(int userId){
+        try{
+            String query = "select * from users where user_id = ?";
+            Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, userId);
+            ResultSet res = stmt.executeQuery();
+            if(!res.isBeforeFirst()){
+                System.out.println("Invalid User...");
+                System.exit(0);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public int displayMovies(String city){
         try{
             String query = """
@@ -54,10 +72,10 @@ public class BookMyMoviesSystem {
 
             if(!res.isBeforeFirst()){
                 System.out.println("No movies available in " + city);
-                return -1;
+                System.exit(0);
             }
-            System.out.println("Available Movies");
 
+            System.out.println("Available Movies");
             System.out.printf("+------+------------------------------+------------+-----------------+\n");
             System.out.printf("| %-4s | %-28s | %-10s | %-15s |\n", "ID", "Title", "Language", "Genre");
             System.out.printf("+------+------------------------------+------------+-----------------+\n");
@@ -83,7 +101,7 @@ public class BookMyMoviesSystem {
         return -1;
     }
 
-    public boolean displayTheaters(String city, int movieId){
+    public void displayTheaters(String city, int movieId){
         try{
             String query = """
                     SELECT DISTINCT\s
@@ -109,12 +127,16 @@ public class BookMyMoviesSystem {
                 st.setInt(1, movieId);
                 ResultSet result = st.executeQuery();
 
-                String movieName = "";
+                String movieName = null;
                 if(result.next()){
                     movieName = result.getString("title");
                 }
-                System.out.println("No Theaters found in " + city + " showing " + movieName);
-                return false;
+                if(movieName == null){
+                    System.out.println("Invalid Selection...");
+                } else{
+                    System.out.println("No Theaters found in " + city + " showing " + movieName);
+                }
+                System.exit(0);
             }
 
             System.out.printf("%-20s %-20s %-20s %-18s %-15s\n",
@@ -135,7 +157,6 @@ public class BookMyMoviesSystem {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return true;
     }
 
     public void displayShows(int movieId, int theaterId){
@@ -147,14 +168,108 @@ public class BookMyMoviesSystem {
             stmt.setInt(2, theaterId);
             ResultSet res = stmt.executeQuery();
 
-            System.out.println("Available Shows: ");
-            while (res.next()){
-                System.out.println(res.getInt("show_id") + ". " + res.getString("show_timing") + "- total seats available: " + res.getInt("available_seats"));
+            if(!res.isBeforeFirst()){
+                System.out.println("Invalid Theater Selection...");
+                System.exit(0);
             }
+
+            System.out.println("Available Shows");
+            System.out.printf("+---------+---------------------+----------------------+\n");
+            System.out.printf("| %-7s | %-19s | %-20s |\n", "Show ID", "Show Timing", "Available Seats");
+            System.out.printf("+---------+---------------------+----------------------+\n");
+
+            while (res.next()) {
+                int showId = res.getInt("show_id");
+                String showTiming = res.getString("show_timing");
+                int availableSeats = res.getInt("available_seats");
+
+                System.out.printf("| %-7d | %-19s | %-20d |\n", showId, showTiming, availableSeats);
+            }
+
+            System.out.printf("+---------+---------------------+----------------------+\n");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public HashSet<Integer> displayAvailableSeats(int showId, int theaterId){
+        HashSet<Integer> validSeats = new HashSet<>();
+        try {
+            String query = "SELECT s.seat_id, s.seat_number FROM seats s JOIN shows ON shows.show_id = s.show_id WHERE s.show_id = ? AND is_booked = 0 AND shows.theater_id = ?";
+            Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, showId);
+            stmt.setInt(2, theaterId);
+            ResultSet res = stmt.executeQuery();
+
+            if (!res.isBeforeFirst()) {
+                System.out.println("No seats available for this show.");
+                System.exit(0);
+            }
+
+            System.out.println("Available Seats:");
+            System.out.printf("+----------+--------------+\n");
+            System.out.printf("| %-8s | %-12s |\n", "Seat ID", "Seat Number");
+            System.out.printf("+----------+--------------+\n");
+
+            while (res.next()) {
+                int seatId = res.getInt("seat_id");
+                String seatNumber = res.getString("seat_number");
+                validSeats.add(seatId);
+                System.out.printf("| %-8d | %-12s |\n", seatId, seatNumber);
+            }
+
+            System.out.printf("+----------+--------------+\n");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return validSeats;
+    }
+
+    public List<Integer> selectSeats(HashSet<Integer> validSeats){
+        List<Integer> selectedSeatsById = new ArrayList<>();
+        do{
+            System.out.println("Select the next Seat Id. Press 0 when you're ready to book.");
+            int currSeat = sc.nextInt();
+            if(currSeat == 0){
+                return selectedSeatsById;
+            }
+            if(!validSeats.contains(currSeat)){
+                System.out.println("Enter a valid seat Id");
+                continue;
+            }
+            if(selectedSeatsById.contains(currSeat)){
+                continue;
+            }
+            selectedSeatsById.add(currSeat);
+        } while(true);
+    }
+
+    public List<String> findSeatsById(List<Integer> seats){
+        List<String> seatNumbers = new ArrayList<String>();
+
+        if (seats == null || seats.isEmpty()) {
+            return seatNumbers;
+        }
+
+        try {
+            Connection conn = DatabaseConfig.getConnection();
+            String query = "SELECT seat_number FROM seats WHERE seat_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+
+            for (int seatId : seats) {
+                stmt.setInt(1, seatId);
+                ResultSet res = stmt.executeQuery();
+                if (res.next()) {
+                    seatNumbers.add(res.getString("seat_number"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return seatNumbers;
     }
 
     public void bookTicket(int userId, int showId, List<String> selectedSeats){
